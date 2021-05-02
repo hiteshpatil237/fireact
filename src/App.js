df032ffb711030a636c50d4fe56d7f5c41cf7337
hiteshpatil237 <hiteshpatil237@gmail.com>
2021-05-02 12:46:44 +0530

    Set up the signup page and enabled email login in firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import './App.css'
+import './firebase/config'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <div className="app">
+        <div className="ui grid container">
+          <Switch>
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
